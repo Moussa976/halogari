cab59cff58a14fd6d4cea12e90e5b4762f2fe50f
Moussa INSSA <40218906+Moussa976@users.noreply.github.com>
2025-07-17 23:14:48 +0300

    Version .8

--- src/Service/AfficheService.php
+++ src/Service/AfficheService.php
@@ -108,7 +108,7 @@ class AfficheService
 
         $fileName = 'trajet_' . uniqid() . '.png';
         $path = $this->outputDir . '/' . $fileName;
-        dd($path); 
+        // dd($path); 
         $image->save($path);
 
         return '/uploads/affiches/' . $fileName;
